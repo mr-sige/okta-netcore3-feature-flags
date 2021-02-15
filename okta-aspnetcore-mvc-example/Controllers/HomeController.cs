9e76a5e9afba4fdac1bfe377205104b52a761df1
sige <sige@configcat.com>
2021-02-15 22:30:06 +0100

    adding feature flag logic

--- okta-aspnetcore-mvc-example/Controllers/HomeController.cs
+++ okta-aspnetcore-mvc-example/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using ConfigCat.Client;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using okta_aspnetcore_mvc_example.Models;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace okta_aspnetcore_mvc_example.Controllers
 {
@@ -17,7 +20,11 @@ namespace okta_aspnetcore_mvc_example.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var userEmail = HttpContext.User.Claims.Where(claim => claim.Type == "email").Select(claim => claim.Value).FirstOrDefault();
+            var user = new User(userEmail) {Email = userEmail};
+            var client = new ConfigCatClient("tNHYCC8Nm0OPXt2LxXT4zQ/k-5ZmLLd10isguXVF6PrTw");
+            var twitterFeedVisible = client.GetValue("twitterFeedVisible", false, user);
+            return View(twitterFeedVisible);
         }
 
         public IActionResult Privacy()
@@ -34,7 +41,7 @@ namespace okta_aspnetcore_mvc_example.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
